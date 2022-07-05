5cca652912d71049c18827ef747d1253d2758c8e
Marco Braccini <marco.braccini@live.it>
2022-07-05 16:51:03 +0200

    Program ok - To do: show ticket button

--- js/main.js
+++ js/main.js
@@ -1,71 +1,35 @@
 const user = document.getElementById('user');
-const km = document.getElementById('km');
+let km = document.getElementById('km');
 const age = document.getElementById('age');
 
-//console.log(user.value);
-console.log(km.value);
-//console.log(age.value);
+km = parseInt(km.value);
 
-const slotUser = document.querySelector('#ticket ul li:nth-child(1)');
-const slotAge = document.querySelector('#ticket ul li:nth-child(2)');
-let slotSeat = document.querySelector('#ticket ul li:nth-child(3)');
-let slotID = document.querySelector('#ticket ul li:nth-child(4)');
-let slotPrice = document.querySelector('#ticket ul li:nth-child(5)');
-
-slotSeat = Math.floor((Math.random() * 10) + 1);
-slotID = Math.floor((Math.random() * 10000) + 90000);
-
-console.log(user.value);
-console.log(age.value);
-console.log(slotSeat);
-console.log(slotID);
-//console.log(slotPrice);
-
+/*
 const btnGen = document.getElementById('btn-generate');
 btnGen.addEventListener('click',
     function(){
         const ticket = document.getElementById('ticket');
-    
+        
+        ticket.classList.add('.show-ticket');
     }
 )
-
-/*
-alert('Benvenuto, compra ora il tuo biglietto del treno!')
-
-const route = parseInt(prompt('Quanti km vuoi percorrere?'));
-console.log('KM da percorrere: ' + route);
-
-const birthYear = parseInt(prompt('In che anno sei nato?'));
-console.log('Anno di nascita: ' + birthYear);
-
-const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-console.log('Anno in corso: ' + currentYear);
-
-let years = parseInt(currentYear - birthYear);
-console.log('Età: ' + years + ' anni');
-
-let priceDemo = parseFloat(0.21 * route).toFixed(2);
-console.log('Prezzo biglietto senza sconto: ' + priceDemo + ' €');
-
-if (years < 18) {
-    price = new Intl.NumberFormat('it-IT', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format((0.21 * route) - ((20 / 100) * (0.21 * route)));
-    alert(`
-    Il prezzo del biglietto YOUNG è: ${price}
-    Buon viaggio!
-    `);
-} else if (years >= 65) {
-    price = new Intl.NumberFormat('it-IT', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format((0.21 * route) - ((40 / 100) * (0.21 * route)));
-    alert(`
-    Il prezzo del biglietto OVER65 è: ${price}
-    Buon viaggio!
-    `);
-}
-else {
-    price = new Intl.NumberFormat('it-IT', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format(0.21 * route);
-    alert(`
-    Il prezzo del biglietto è: ${price}
-    Buon viaggio!
-    `);
-}
 */
+
+let slotUser = document.querySelector('#ticket ul li:nth-child(1)');
+let slotAge = document.querySelector('#ticket ul li:nth-child(2)');
+let slotSeat = document.querySelector('#ticket ul li:nth-child(3)');
+let slotID = document.querySelector('#ticket ul li:nth-child(4)');
+let slotPrice = document.querySelector('#ticket ul li:nth-child(5)');
+
+slotUser = slotUser.innerHTML = (user.value);
+slotAge = slotAge.innerHTML = (age.value);
+slotSeat = slotSeat.innerHTML = (Math.floor((Math.random() * 10) + 1));
+slotID = slotID.innerHTML = (Math.floor((Math.random() * 10000) + 90000));
+
+if (slotAge == ('Biglietto Standard')) {
+    slotPrice.innerHTML = new Intl.NumberFormat('it-IT', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format(0.21 * km);
+} else if (slotAge == ('Biglietto Young')) {
+    slotPrice.innerHTML = new Intl.NumberFormat('it-IT', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format((0.21 * km) - ((20 / 100) * (0.21 * km)));
+} else {
+    slotPrice.innerHTML = new Intl.NumberFormat('it-IT', {style: 'currency', currency: 'EUR', minimumFractionDigits: 2}).format((0.21 * km) - ((40 / 100) * (0.21 * km)));
+}
